2a5b7817d84af257d7c3101917c3c1a79ce4e184
qiyu <qiyu@p1.com>
2020-04-16 11:37:28 +0800

    Initial Commit

---
+++ Photos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photos
+//
+//  Created by qiyu on 2020/4/16.
+//  Copyright © 2020 com.qiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Photos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photos
+//
+//  Created by qiyu on 2020/4/16.
+//  Copyright © 2020 com.qiyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
